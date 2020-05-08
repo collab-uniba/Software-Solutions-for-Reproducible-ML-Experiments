d436fb4ca21ac6cbc169c06edf753c821ec11ce6
Luigi Quaranta <quarantalgu@gmail.com>
2020-05-08 11:43:23 +0200

    Adds bash script to generate a new test environment.

---
+++ new_tool.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Call convetion
+# new_tool <tool_name>
+
+mkdir tools/$1
+
+cp "kernels/Kaggle - Intro to ML.ipynb" "tools/${1}/Kaggle - Intro to ML.ipynb"
+
+cd tools/$1
+virtualenv "${1}_env"
+source "${1}_env/bin/activate"
+pip install -r ../../basic_requirements.txt
+
+echo "Environment for '${1}' is ready."
