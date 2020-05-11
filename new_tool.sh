0bcd182c0d123dada09281794e6b108e2767e41f
Luigi Quaranta <quarantalgu@gmail.com>
2020-05-11 11:37:55 +0200

    Differenciates between script-oriented tools and notebook-oriented ones.

--- new_tool.sh
+++ new_tool.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
 # Call convetion
-# new_tool <tool_name>
+# new_tool <tool_name> <notebook>|<script>
 
 mkdir tools/$1
 
-cp "kernels/SampleNotebook_for_APItools.ipynb" "tools/${1}/Kaggle - Intro to ML.ipynb"
+# if [$2 = "script"]; then
+# fi
+
+if [$2 = "notebook"]; then
+    # Copy example notebook
+    cp "kernels/SampleNotebook_for_APItools.ipynb" "tools/${1}/Kaggle - Intro to ML.ipynb"
+fi
 
 cd tools/$1
+
+# Create and activate virtual environment
 virtualenv "${1}_env"
 source "${1}_env/bin/activate"
+
+# Install python requirements that are commont to all reviews
 pip install -r ../../basic_requirements.txt
 
-ipython kernel install --user --name="${1}_env"
+if [$2 = "notebook"]; then
+
+    # Install Jupyter Notebook
+    pip install notebook
+
+    # Deactivate and activate again to update the path
+    deactivate
+    source "${1}_env/bin/activate"
+
+    # Install kernelspec
+    ipython kernel install --user --name="${1}_env"
+fi
 
 echo "Environment for '${1}' is ready."
